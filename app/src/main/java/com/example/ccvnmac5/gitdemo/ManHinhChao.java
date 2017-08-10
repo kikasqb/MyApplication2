de06cb44ed48cd5560c6d3d9d2a708d889ada8bb
CCVN Mac 5 <ccvnmac5@CCVNs-Mac-mini.local>
2017-08-10 11:34:14 +0700

    em commit len master

---
+++ app/src/main/java/com/example/ccvnmac5/gitdemo/ManHinhChao.java
@@ -0,0 +1,17 @@
+package com.example.ccvnmac5.gitdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ccvnmac5 on 8/10/17.
+ */
+
+public class ManHinhChao extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.demo_layout);
+    }
+}
